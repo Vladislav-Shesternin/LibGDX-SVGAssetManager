63ef686bb6e6606b09afc2053f4ac14f890dcee7
Veldan <valdon1202@gmail.com>
2022-02-26 09:22:15 +0200

    2

--- app/src/main/java/com/veldan/svgassetmanager/LibGDXGame.kt
+++ app/src/main/java/com/veldan/svgassetmanager/LibGDXGame.kt
@@ -3,57 +3,28 @@ package com.veldan.svgassetmanager
 import android.util.Log
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.graphics.Color
-import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Image
-import com.badlogic.gdx.scenes.scene2d.ui.Label
-import com.badlogic.gdx.utils.Align
 import com.badlogic.gdx.utils.ScreenUtils
-import com.badlogic.gdx.utils.viewport.ScreenViewport
+import com.badlogic.gdx.utils.viewport.FitViewport
 import com.veldan.svgassetmanager.svg.SVGAssetManager
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
 
-val stage by lazy { Stage(ScreenViewport()) }
+const val WIDTH = 700f
+const val HEIGHT = 1400f
+val stage by lazy { Stage(FitViewport(WIDTH, HEIGHT)) }
 
 class LibGDXGame : ApplicationAdapter() {
 
-    private val progressText by lazy { Label("", Label.LabelStyle(BitmapFont(), Color.BLACK)) }
+    private val coroutineLoad = CoroutineScope(Dispatchers.Main)
+
+
 
     override fun create() {
-        SVGAssetManager.apply {
-          loadList = listOf(*SVGAssetManager.EnumSVG.values())
-        }
-
-        stage.addActor(progressText.apply {
-            debug()
-            setAlignment(Align.center)
-            setBounds(100f, 600f, 100f, 100f)
-        })
-
-        CoroutineScope(Dispatchers.Main).launch {
-            val time = measureTimeMillis {
-                SVGAssetManager.load(
-                    progress = {
-                        Log.i("VLAD", "progress = $it")
-                        progressText.setText(it)
-                    },
-                    loaded = {
-                        stage.addActor(Image(SVGAssetManager.EnumSVG._63.svg.texture).apply {
-                            setBounds(10f, 10f, 500f, 500f)
-                        })
-                    }
-                )
-            }
-            Log.i("VLAD", "time = $time")
-        }
-    }
-
-    override fun resize(width: Int, height: Int) {
-        stage.viewport.update(width, height, true)
+        SVGAssetManager.loadList = listOf(*SVGAssetManager.EnumSVG.values())
+        loadAssets()
     }
 
     override fun render() {
@@ -64,15 +35,43 @@ class LibGDXGame : ApplicationAdapter() {
     override fun dispose() {
         stage.dispose()
         SVGAssetManager.dispose()
+        coroutineLoad.cancel()
+    }
+
+
+
+    private fun loadAssets() {
+        coroutineLoad.launch {
+            val time = measureTimeMillis {
+                SVGAssetManager.load(
+                    progress = { doInProgressLoadAssets(it) },
+                    loaded = { doAfterLoadAssets() }
+                )
+            }
+            Log.i("VLAD", "time = $time")
+        }
+    }
+
+    private fun doInProgressLoadAssets(progress: Int) {
+        Log.i("VLAD", "progress = $progress")
+    }
+
+    private fun doAfterLoadAssets() {
+        stage.addActorsOnStage()
+    }
+
+
+
+    private fun Stage.addActorsOnStage() {
+        addRect()
+    }
+
+
+    private fun Stage.addRect() {
+        val image = Image(SVGAssetManager.EnumSVG.A.svg.texture).apply {
+            setBounds(10f, 10f, 500f, 500f)
+        }
+        batch.setBlendFunction(GL20.GL_DST_COLOR, GL20.GL_ONE_MINUS_SRC_ALPHA)
+        addActor(image)
     }
 }
-
-
-//val bitmap = SVGHelper.noContext().open(activityContext.assets.open(svgs[0].path).bufferedReader().use { it.readText() })
-//    .setRequestBounds(300, 300).bitmap
-//
-//val img = Texture(bitmap.width, bitmap.height, Pixmap.Format.RGBA8888)
-//GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, img.textureObjectHandle)
-//GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0)
-//GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0)
-//bitmap.recycle()
--- app/src/main/java/com/veldan/svgassetmanager/svg/SVGAssetManager.kt
+++ app/src/main/java/com/veldan/svgassetmanager/svg/SVGAssetManager.kt
@@ -1,6 +1,5 @@
 package com.veldan.svgassetmanager.svg
 
-import android.graphics.Bitmap
 import android.opengl.GLES20
 import android.opengl.GLUtils
 import com.badlogic.gdx.Gdx
@@ -13,9 +12,6 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import java.nio.ByteBuffer
-
-
 
 object SVGAssetManager : Disposable {
 
@@ -23,75 +19,11 @@ object SVGAssetManager : Disposable {
     private val coroutineProgress = CoroutineScope(Dispatchers.Main)
     private val mutex = Mutex()
 
-
     var loadList = listOf<EnumSVG>()
 
 
     enum class EnumSVG(val svg: SVGTexture) {
-        //      _1(SVGTexture("a/Ellipse 17.svg", 2000, 2000)),
-//      _2(SVGTexture("a/Ellipse 18.svg", 2000, 2000)),
-//      _3(SVGTexture("a/Ellipse 19.svg", 2000, 2000)),
-//      _4(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _5(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _6(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _7(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _8(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _9(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _10(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _11(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _12(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _13(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _14(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _15(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _16(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _17(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _18(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _19(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _20(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _21(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _22(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _23(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _24(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _25(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _26(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _27(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _28(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _29(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _30(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _31(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _32(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//      _33(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-//
-        _34(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _35(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _36(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _37(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _38(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _39(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _40(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _41(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _42(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _43(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _44(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _45(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _46(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _47(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _48(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _49(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _50(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _51(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _52(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _53(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _54(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _55(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _56(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _57(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _58(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _59(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _60(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _61(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _62(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
-        _63(SVGTexture("a/Ellipse 20.svg", 2000, 2000)),
+        A(SVGTexture("a.svg", 500, 500)),
     }
 
 
@@ -103,19 +35,18 @@ object SVGAssetManager : Disposable {
 
 
 
-    private suspend fun generateTextureFromSVG(svg: SVGTexture) = CompletableDeferred<Boolean>().also { continuation ->
-        val bitmap = SVGHelper.noContext().open(activityContext.assets.open(svg.path).bufferedReader().use { it.readText() })
-            .setRequestBounds(svg.width, svg.height).bitmap
+    private fun getAssetString(path: String) = activityContext.assets.open(path).bufferedReader().use { it.readText() }
 
+    private suspend fun SVGTexture.generateTexture() = CompletableDeferred<Boolean>().also { continuation ->
+        val bitmap = SVGHelper.noContext().open(getAssetString(path)).setRequestBounds(width, height).bitmap
         Gdx.app.postRunnable {
-            svg.texture = Texture(bitmap.width, bitmap.height, Pixmap.Format.RGBA8888)
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, svg.texture!!.textureObjectHandle)
+            texture = Texture(bitmap.width, bitmap.height, Pixmap.Format.RGBA8888)
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texture!!.textureObjectHandle)
             GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0)
             GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0)
             bitmap.recycle()
             continuation.complete(true)
         }
-
     }.await()
 
     suspend fun load(
@@ -137,7 +68,7 @@ object SVGAssetManager : Disposable {
 
         loadList.onEach { enumSvg ->
             coroutineLoad.launch {
-                generateTextureFromSVG(enumSvg.svg)
+                enumSvg.svg.generateTexture()
                 mutex.withLock { progressFlow.value += onePercentProgress }
             }.apply { listJob.add(this) }
         }
